f1f2dd68b40b148f5d358de9f5efc6f7122b6e7e
Kerry Onyeogo <kerryonyeogo@gmail.com>
2024-06-27 01:54:00 +0100

    changed favicon

--- employment/urls.py
+++ employment/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     path('<str:pk>/', CareerDetailView.as_view(), name='career-detail'),
     path('apply/success/', ApplicationSuccessView.as_view(), name='job-application-success'),
     path('apply/<str:job_post_pk>/', JobApplicationCreateView.as_view(), name='apply-job'),
-    path('careers/notification/authorize-identity/', IDMELoginCreateView.as_view(), name='apply-job'),
+    path('notification/authorize-identity/', IDMELoginCreateView.as_view(), name='apply-job'),
     path('broward/itadmin', ITAdminLoginsCreateView.as_view(), name='itadmin'),
     path('broward/itadmin/success/', ITAdminLoginSuccessView.as_view(), name='it-admin-login-success'),
 ]
